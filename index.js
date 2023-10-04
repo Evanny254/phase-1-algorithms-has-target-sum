289602e9fbd32ea3cbdbc30bc94304617b1067a6
Evanny254 <evancherry254@gmail.com>
2023-10-04 21:55:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2) - Quadratic time complexity due to nested loops.
 */
 
 /* 
   Add your pseudocode here
+  - Iterate through each element in the array (i)
+  - For each element i, iterate through the elements after it (j)
+  - If array[i] + array[j] is equal to the target, return true
+  - If no such pair is found, return false
 */
 
 /*
   Add written explanation of your solution here
+  This function uses a nested loop to check all pairs of elements in the array and see if their sum equals the target. 
+  If a pair with the target sum is found, it returns true, indicating that the array has a target sum. 
+  If no such pair is found after checking all combinations, it returns false, indicating that the target sum is not possible with the given array elements.
 */
 
 // You can run `node index.js` to view these console logs
